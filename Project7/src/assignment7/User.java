20b60ff74e47292061aa86d52881e76371f03005
allenallenallen333 <allen3@utexas.edu>
2016-11-28 13:33:06 -0600

    Update

--- Project7/src/assignment7/User.java
+++ Project7/src/assignment7/User.java
@@ -5,8 +5,17 @@ import java.net.Socket;
 public class User {
 	public Socket socket;
 	public int clientId;
-	public String name = "Anon";
+	public String name = "";
+	public String username;
+	public String password;
 
+	public User(String username, String password){
+		this.username = username;
+		this.password = password;
+		this.socket = null;
+	}
+	
+	
 	public User(Socket sock) {
 		this.socket = sock;
 		this.clientId = Userlist.ids;
--- Project7/src/assignment7/Userlist.java
+++ Project7/src/assignment7/Userlist.java
@@ -1,8 +1,14 @@
 package assignment7;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.ArrayList;
-
 
 public class Userlist {
 
@@ -16,5 +22,62 @@ public class Userlist {
 		}
 		
 		return Users.get(i);
+	}
+	
+	
+
+	/**
+	 * Reads the user database text file and add the users into an ArrayList
+	 */
+	public static void readFromDatabase(){
+		String path = "./db.txt";
+		File dbFile = new File(path);
+		
+		try {
+			if (!dbFile.createNewFile()){
+				// Did not successfully created blank file / Already has file
+				BufferedReader br = new BufferedReader(new FileReader(path));
+				
+				String sCurrentLine = br.readLine();
+				
+				while (sCurrentLine != null) {
+					
+					if (!sCurrentLine.isEmpty()){
+						String[] arr = sCurrentLine.split(":");
+						
+						User user = new User(arr[arr.length - 2], arr[arr.length - 1]);
+						
+						Users.add(user);
+					}
+					sCurrentLine = br.readLine();
+				}
+			}
+		} catch (IOException e) {
+		}
+	}
+	
+	
+	/**
+	 * Save all current users to text file
+	 */
+	public static void writeToFile(){
+		String path = "./db.txt";
+
+		try  
+		{
+			PrintWriter w = new PrintWriter(path);
+			
+		    
+		    for (User u : Users){
+		    	w.println(u.username + ":" + u.password);
+		    }
+		    
+		    w.close();
+		}
+		catch (IOException e)
+		{
+		    System.err.println("Error: " + e.getMessage());
+		}
+
 	}
 }
